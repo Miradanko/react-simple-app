aeabc43b26b6c2dc9d217dbde456a15df8c2f615
Tsiupiak Myroslava <miradanko21@gmail.com>
2020-07-24 15:29:39 +0300

    Updates

---
+++ precache-manifest.3a644c0d8f3f008a54a1b6906cd69fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672180fe5ec172a9821016b64c33d4d6",
+    "url": "/react-simple-app/index.html"
+  },
+  {
+    "revision": "d5bf99ec7328323f41a3",
+    "url": "/react-simple-app/static/css/main.8c2c41cb.chunk.css"
+  },
+  {
+    "revision": "eba6e68a3c25e09716ba",
+    "url": "/react-simple-app/static/js/2.24019a5e.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/react-simple-app/static/js/2.24019a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bf99ec7328323f41a3",
+    "url": "/react-simple-app/static/js/main.ee4ff602.chunk.js"
+  },
+  {
+    "revision": "1bf64d1f7d6abcab3aec",
+    "url": "/react-simple-app/static/js/runtime-main.d6c78833.js"
+  }
+]);
